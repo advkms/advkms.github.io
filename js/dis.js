ce16c14641441b630adfa2c8ad4e820d4aeda1e5
gauthamzz <thabeatsz@gmail.com>
2018-01-06 17:22:45 +0530

    add website for adv kms 1.0

---
+++ js/dis.js
@@ -0,0 +1,10 @@
+var $$cookieDisclaimer = document.querySelector('.js-cookie-disclaimer');
+
+if (!localStorage.getItem('cookieDisclaimer')) {
+  $$cookieDisclaimer.classList.add('is-active');
+}
+
+$$cookieDisclaimer.querySelector('button').addEventListener('click', function () {
+  localStorage.setItem('cookieDisclaimer', true);
+  $$cookieDisclaimer.classList.remove('is-active');
+});
